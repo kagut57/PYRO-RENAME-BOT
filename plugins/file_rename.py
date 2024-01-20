3f358f32ff8290f958ef60d90d98d809f5e79cdc
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-01-20 19:42:16 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -71,7 +71,7 @@ async def refunc(client, message):
 
 
 @Client.on_callback_query(filters.regex("upload"))
-async def doc(bot, update):    
+async def doc(bot, update, message):    
     new_name = update.message.text
     new_filename = new_name.split(":-")[1]
     file_path = f"downloads/{new_filename}"
