295e174d4803a6ffbcf0aa9cfca9cbf6661d16e9
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-01-20 19:18:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://graph.org//file/20df394e337acdfcc4b43.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5591954930').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "Wizard_Bots") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001723894782))
+    #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001723894782))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "False"))
